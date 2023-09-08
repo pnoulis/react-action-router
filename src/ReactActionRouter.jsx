082da6fccd1b6ed1b1aae373e5ffdfc0ea196dfa
pnoul <pavlos.noulis@gmail.com>
2023-09-08 16:48:40 +0300

    Brainstorming ideas

--- src/ReactActionRouter.jsx
+++ src/ReactActionRouter.jsx
@@ -1,5 +1,17 @@
+import * as React from "react";
+
+function createActionRouter(routes = [], options = {}) {
+  return () => {
+    const [stack, setStack] = React.useState(routes);
+    function back() {}
+    function forward() {}
+  };
+}
+
 function ReactActionRouter() {
   return <div>yolo</div>;
 }
+
+// React router is a function, which
 
 export { ReactActionRouter };
